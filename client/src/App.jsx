c7e6a92e49e6aaab5c5976785aa1a9966b0b0f31
Rajdev Yadav <ryindia2023@gmail.com>
2023-10-11 01:08:50 +0530

    React Vite File Cleaned Up

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './assets/logo.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, corrupti.</h1>
+        <img src={logo} alt="" />
+    </>
+  )
+}
+
+export default App
